1df5a2ba705c4be8ab9a44e015d620bdc0b96c48
Sachin987635 <sachindemo30@gmail.com>
2024-03-03 12:51:29 +0530

    Code push

--- Zero/src/Day3Oct2/Assignment2.java
+++ Zero/src/Day3Oct2/Assignment2.java
@@ -12,19 +12,19 @@ public class Assignment2 {
 	}
 	public void m1(int a) {
 		this.m1(1,2,3);
-		System.out.println("1 parameterized method");
+		System.out.println("1 parameterized method Test");
 	}
 	public void m1(int a,int b) {
 		this.m1(1, 2, 3, 4);
-		System.out.println("2 parameterized method");
+		System.out.println("2 parameterized method Test");
 	}
 	public void m1(int a,int b,int c) {
 		this.m1();
-		System.out.println("3 parameterized method");
+		System.out.println("3 parameterized method Test");
 	}
 	public void m1(int a,int b,int c,int d) {
 		this.m1(1);
-		System.out.println("4 parameterized method");
+		System.out.println("4 parameterized method Test");
 	}
 	public static void main(String[] args) {
 		Assignment2 obj=new Assignment2();
